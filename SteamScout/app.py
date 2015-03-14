4224525a0e516bc039ad77501bc5441d7c5e30c6
Charles C. Lee <charleschanlee@gmail.com>
2015-03-14 21:58:02 +0000

    Create models

--- SteamScout/app.py
+++ SteamScout/app.py
@@ -4,61 +4,106 @@ from flask import (
     )
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from flask.ext.openid import OpenID
 from flask.ext.login import LoginManager
+import requests
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config')
-    #app.config['SQLALCHEMY_DATABASE_URI'] = #
-    Bootstrap(app)
-    db = SQLAlchemy(app)
-    oid = OpenID(app)
-    
-    
-    @app.before_request
-    def lookup_current_user():
-        g.user = None
-        if 'openid' in session:
-            openid = session['openid']
-            g.user = User.query.filter_by(openid=openid).first()  # I think we need to import user from models
-    
-    #routes
-    @app.route('/')
-    def home():
-        return render_template('home.html')
-    
-    @app.route('/games')
-    def games():
-        # page that shows all the games.
-        return render_template('games.html')
+app = Flask(__name__)
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/steamscout.db'
+db = SQLAlchemy(app)
+Bootstrap(app)
 
-    @app.route('/developers')
-    def show_developors():
-        # page that shows all the games.
-        return render_template('developers.html')
+################################### MODELS #####################################
+#A model which will store the users steam ID for Sign in
+class User(db.Model):
+    __tablename__= 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
 
-    @app.route('/contact')
-    def contact():
-        # page that shows all the games.
-        return render_template('contact.html')
-    
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        return render_template('login.html')
+    def __init__(self,name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
         
-    @app.route('/signup', methods=['GET', 'POST'])
-    def signup():
-        return render_template('signup.html')        
+    def __repr__(self):
+        return '<name: {}>'.format(self.name)
+
+class Preferences(db.Model):
+    __tablename__= 'preferences'
+    preference_id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.user_id'))
+    game_id=db.Column(db.Integer)
+    threshold_amount=db.Column(db.Float)
+    threshold_percent=db.Column(db.Float)
     
-    @app.route('/settings')
-    def settings():
-        pass
+    def __init__(self, user_id, game_id, threshold_amount, threshold_percent):
+        self.user_id = user_id
+        self.game_id = game_id
+        self.threshhold_amountt = threshold_amount
+        self.threshold_percent = threshold_percent
+
+class Games(db.Model):
+    __tablename__='games'
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, unique=True)
+    game_name = db.Column(db.String, unique=True)
+
+################################# VIEWS ########################################
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/games')
+def games():
+    # page that shows all the games.
+    games_request = requests.get('http://api.steampowered.com/ISteamApps/GetAppList/v0001')
+    all_games = [game for game in (games_request.json()["applist"]["apps"]["app"])]
+    return render_template('games.html', all_games=all_games)
+
+@app.route('/developers')
+def show_developors():
+    # page that shows all the games.
+    return render_template('developers.html')
+
+@app.route('/contact')
+def contact():
+    # page that shows all the games.
+    return render_template('contact.html')
+
+@app.route('/settings')
+def settings():
+    pass
+
+# Log in / Log out
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     pass
+
+# @app.before_request
+# def before_request():
+#     g.user = None
+#     if 'user_id' in session:
+#         g.user = User.query.get(session['user_id'])
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('user_id', None)
+#     return redirect(oid.get_next_url())
+
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     return render_template('signup.html')        
+
     
-    return app
+    
+
 
 if __name__ == "__main__":
-    create_app().run(host='0.0.0.0', port=8080, debug=True) # We have to remember to change debug = True back to False if we deply to heruku
-
+    # create_app().run(host='0.0.0.0', port=8080, debug=True) # We have to remember to change debug = True back to False if we deply to heruku    
+    app.run(host='0.0.0.0', port=8080, debug=True)
     # site url: https://002-pyp-demoday-g1-chanchar.c9.io
+    db.create_all()
--- SteamScout/models.py
+++ SteamScout/models.py
@@ -1,38 +1,9 @@
-from app import db, oid
-from flask import g, session
-
-#A model which will store the users steam ID for Sign in
-class User(db.Model):
-    __tablename__= 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    steam_id = db.Column(db.String(40))
-    nickname = db.String(80)
-    
-    def __init__(self, steam_id, nickname):
-        self.steam_id = steam_id
-        self.nickname = nickname
-    
-    def __repr__(self):
-        return '<nickname: {}>'.format(self.nickname)
 
 
 
-class Preferences(db.Model):
-    __tablename__= 'preferences'
-    user_id=db.Column(db.Integer,db.ForeignKey('User.id'), primary_key=True)
-    user = relatiochip(User, backref=backref(
-    game_id=db.Column(db.Integer)
-    thresh_amt=db.Column(db.float)
-    thresh_pct=db.Colum
+
+
+
+        
     
     
-#Add user to db if he does not exist in db    
-""""@staticmethod
-    def get_or_create(steam_id):
-        rv = User.query.filter_by(steam_id=steam_id).first() # not quite sure what the issue is here
-        if rv is None:
-            rv = User()
-            rv.steam_id = steam_id
-            db.session.add(rv)
-        return rv
-        """

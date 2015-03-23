92a4ffd697a60dfb9bedfc7b5ff9f4cb48d11a50
Charles C. Lee <charleschanlee@gmail.com>
2015-03-23 02:43:53 +0000

    fixed 404

--- SteamScout/__init__.py
+++ SteamScout/__init__.py
@@ -28,5 +28,5 @@ def internal_server_error(e):
     return render_template("500.html")
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(e):
     return render_template("404.html")

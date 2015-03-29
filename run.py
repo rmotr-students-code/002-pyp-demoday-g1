db49c936ce7e024fa5bac06acd105a54f496843f
Charles C. Lee <charleschanlee@gmail.com>
2015-03-29 22:56:58 +0000

    changed run.py

--- run.py
+++ run.py
@@ -2,8 +2,9 @@
 from SteamScout import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
+    
     # site url: https://002-pyp-demoday-g1-chanchar.c9.io
 
     # server error: Port being used ... yada yada

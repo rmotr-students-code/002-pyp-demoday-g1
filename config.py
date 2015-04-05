182bb8207655f32d8e24c7df4ad93da282a988eb
Charles C. Lee <charleschanlee@gmail.com>
2015-04-05 04:25:08 +0000

    Added heroku's db config

--- config.py
+++ config.py
@@ -52,6 +52,7 @@ class ProductionConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     PORT = None
     HOST = None
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEBUG = True
